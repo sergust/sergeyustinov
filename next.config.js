88756585fb8c0ee6354dcce06c19f592c75a1876
Sergei Ustinov <sergey.ustinov@sensand.com>
2022-06-05 11:56:32 +1000

    code style config / run prettier

--- next.config.js
+++ next.config.js
@@ -1,15 +1,15 @@
 /** @type {import('next').NextConfig} */
 
-const path = require("path");
+const path = require('path')
 
 module.exports = {
   sassOptions: {
-    includePaths: [path.join(__dirname, "styles")],
+    includePaths: [path.join(__dirname, 'styles')],
   },
-};
+}
 
 const nextConfig = {
   reactStrictMode: true,
-};
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
