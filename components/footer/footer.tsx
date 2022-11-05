fcfeef8d7b08eed172dca2eae0253d1ec4fc92e5
Sergei Ustinov <sergey.ustinov@sensand.com>
2022-11-05 16:13:10 +1100

    Refactor global styles

--- components/footer/footer.tsx
+++ components/footer/footer.tsx
@@ -3,11 +3,9 @@ import { NextComponentType } from 'next'
 const Footer: NextComponentType = () => {
   return (
     <div className="flex w-full justify-center mt-32">
-      <div>
-        <span className="font-mono">
-          ©{new Date().getFullYear()} Made in Sydney with ❤️
-        </span>
-      </div>
+      <span className="font-mono">
+        ©{new Date().getFullYear()} Made in Sydney with ❤️
+      </span>
     </div>
   )
 }
