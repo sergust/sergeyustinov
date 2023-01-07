cdc64bb0fa6d89e63f100a245d4a54f454cf2fde
Sergei Ustinov <sergey.ustinov@sensand.com>
2023-01-07 14:07:24 +1100

    Add clickable logo

--- components/header/the-logo.tsx
+++ components/header/the-logo.tsx
@@ -1,9 +1,17 @@
 import type { NextPage } from 'next'
+import { useRouter } from 'next/router'
 
-const Logo: NextPage = () => (
-  <div className="text-2xl text-green font-sans font-semibold subpixel-antialiased tracking-wide border border-green p-2">
-    SU
-  </div>
-)
+const Logo: NextPage = () => {
+  const router = useRouter()
+  const handleClick = () => router.push('/')
+  return (
+    <div
+      onClick={handleClick}
+      className="cursor-pointer text-4xl text-green font-sans font-semibold subpixel-antialiased tracking-wide"
+    >
+      SU
+    </div>
+  )
+}
 
 export default Logo
